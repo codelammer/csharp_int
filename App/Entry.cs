de9c91f26863795e94e8bd824c1ba058f1507716
codelammer <briancheruiyott@gmail.com>
2024-08-16 23:58:25 +0300

    init app, solve integer problem

---
+++ App/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CsharpInterview;
+
+namespace CsharpInterview
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            IntegerProblem.descendingNonDuplicateList();
+            Console.WriteLine("Reading something from Entry.");
+        }
+    }
+}
---
+++ IntegerProblem/IntegerProblem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CsharpInterview
+{
+    public class IntegerProblem
+    {
+        public static void descendingNonDuplicateList()
+        {
+            //get input and it should be separated by spaces
+            Console.WriteLine("Enter a list of integers separated by spaces:");
+            string? input = Console.ReadLine();
+
+            if (input != null)
+            {
+                //this is easier --> just split map each by parsing to int and convert it back to list
+                List<int> integerList = input.Split(" ")
+                                             .Select(int.Parse)
+                                             .ToList();
+
+                //remove duplicate values and orderByDes and back to list
+                List<int> uniqueList = integerList.Distinct()
+                                                  .OrderByDescending(x => x)
+                                                  .ToList();
+
+                Console.WriteLine("Showing list sorted in Descending and non-duplicate ");
+                foreach (int number in uniqueList)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No input provided.");
+            }
+        }
+    }
+}

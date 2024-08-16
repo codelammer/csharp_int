98e96409c0eeb940fb179d02a36ec416828e1640
codelammer <briancheruiyott@gmail.com>
2024-08-16 22:12:05 +0300

    add gitignore

--- selenium_tests/obj/Debug/net8.0/selenium_tests.AssemblyInfo.cs
+++ selenium_tests/obj/Debug/net8.0/selenium_tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("selenium_tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef426011b2cc8e2577b0c88bae55e44dd8400068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42d1b4b89e6c7524e2ffe9c0602baede24857e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("selenium_tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("selenium_tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
